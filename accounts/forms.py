8158be6389184e533fa58612e2c06eb41f5f22f8
Ian Abesia <yanabesia21@gmail.com>
2026-07-01 14:27:28 +0800

    Add KlickCebu visual theme to login, registration, and dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,13 +4,19 @@ from .models import User
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control-kc'}))
+    last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control-kc'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control-kc'}))
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control-kc'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control-kc'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control-kc'})
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'dashboard:home'
